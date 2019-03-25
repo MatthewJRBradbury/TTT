61cb708ebbd7cfe7d5bace1da7a2cc2d3c4f767f
Matthew <matthew.j.r.bradbury@gmail.com>
2019-03-25 15:09:39 +1100

    new Game class

--- TTT/src/com/example/ttt/Board.java
+++ TTT/src/com/example/ttt/Board.java
@@ -2,9 +2,5 @@ package com.example.ttt;
 
 public class Board {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 
 }
---
+++ TTT/src/com/example/ttt/Game.java
@@ -0,0 +1,10 @@
+package com.example.ttt;
+
+public class Game {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+
+}
